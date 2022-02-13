515797904e143fcb008a057382521e7647fac0f6
Utsav Kumar <utsavkc24@gmail.com>
2022-02-13 13:28:31 +0530

    Get login user info

--- todos/src/main/java/com/management/todos/TodosApplication.java
+++ todos/src/main/java/com/management/todos/TodosApplication.java
@@ -2,12 +2,13 @@ package com.management.todos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TodosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodosApplication.class, args);
 	}
-
 }
--- todos/src/main/java/com/management/todos/services/TodosService.java
+++ todos/src/main/java/com/management/todos/services/TodosService.java
@@ -11,12 +11,13 @@ import com.management.todos.model.Todos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -79,5 +80,11 @@ public class TodosService {
         Optional<Todos> todo = todoRepository.findById(id);
         todoRepository.delete(todo.get());
         return "redirect:/";
+    }
+
+    @GetMapping("/user")
+    public String getUser(@AuthenticationPrincipal OAuth2User principal, Model model) {
+        model.addAttribute("image", principal.getAttribute("picture"));
+        return "user-image";
     }
 }
